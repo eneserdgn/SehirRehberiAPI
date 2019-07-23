73636110168666d316e4742fb52fc8685b1f3cf9
Enes Erdoğan <enes.erdgn32@gmail.com>
2019-07-23 06:05:54 +0300

    Projenin temelleri
    Asp.net core web uygulamasının oluşturulması.
    Angular projesinin oluşturulması.
    Web APİ Entity Framework ve Angular tarafında konfigurasyon ayarlamalarının yapılması.

---
+++ SehirRehberi.API/SehirRehberi.API/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SehirRehberi.API.Data;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //Bunun sayesinde _context ile veritabanımıza erişip kodlarla işimizi haleldebiliriz
+        private DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult GetValue(int id)
+        {
+            var value = _context.Values.Find(id);
+            return Ok(value);
+        } 
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SehirRehberi.API/SehirRehberi.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Data
+{                   //Context sınıfımızı yazdık
+    public class DataContext:DbContext
+    {                            //Context sınıfımızı yazdık
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+                
+        }
+        //Modeldeki sınıflar ile kod yazarken kullanıcamız isim arasındaki ilişki
+        public DbSet<Value> Values { get; set; }
+    }
+}
